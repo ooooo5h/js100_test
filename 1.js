e66f813477ba585dd89ade8386224579e13f830c
Jeon, Eun Hyung <78359232+ooooo5h@users.noreply.github.com>
2022-05-25 23:27:17 +0900

    #3
    arr은 typeof로 출력시 object가 출력된다.

--- 1.js
+++ 1.js
@@ -12,3 +12,7 @@ var arr = [200, 100, 300];
 arr.splice(2, 0, 10000);
 
 console.log(arr);  // 출력 [200, 100, 10000, 300]되게 해라.
+
+// #3. 변수의 타입
+var arr = [100, 200, 300];
+console.log(typeof(arr));  // 정답 object
