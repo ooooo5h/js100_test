841a50da6fe60d6bf44f77501a0cf3fad9d542b5
Jeon, Eun Hyung <78359232+ooooo5h@users.noreply.github.com>
2022-05-25 23:28:21 +0900

    #4

--- 1.js
+++ 1.js
@@ -16,3 +16,11 @@ console.log(arr);  // 출력 [200, 100, 10000, 300]되게 해라.
 // #3. 변수의 타입
 var arr = [100, 200, 300];
 console.log(typeof(arr));  // 정답 object
+
+// #4. 변수의 타입2
+
+// 다음 변수 a를 `typeof(a)`로 넣었을 때 출력될 값과의 연결이 알맞지 않은 것은?
+// 1)  입력 : a =1,   출력 : number
+// 2)  입력 : a = 2.22,   출력 : boolean   -> Number
+// 3)  입력 : a = 'p',   출력 : string
+// 4)  입력 : a = [1, 2, 3],   출력 : object
