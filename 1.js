e562638b27eb3c181f81b3c3b6c3b6d6e2d6845e
Jeon, Eun Hyung <78359232+ooooo5h@users.noreply.github.com>
2022-05-25 23:37:43 +0900

    #9

--- 1.js
+++ 1.js
@@ -66,3 +66,16 @@ var d = {
 };
 
 // console.log(d['weight']); 의 출력값은 ? 78? 땡
+
+// #9. concat을 활용한 출력 방법
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+var result = year.concat('/',month,'/',day, ' ', hour, ':',minute,':',second)
+
+console.log(result);  // 2019/04/26 11:34:27라고 출력되게하기
+
