bc691075482624c29e8f0681ef049760f975ec29
Jeon, Eun Hyung <78359232+ooooo5h@users.noreply.github.com>
2022-05-26 22:08:37 +0900

    #19

--- 1.js
+++ 1.js
@@ -1,115 +1,115 @@
-// #1. 다음 배열에서 400, 500를 삭제하는 code를 입력하세요.
-var nums = [100, 200, 300, 400, 500];
+// // #1. 다음 배열에서 400, 500를 삭제하는 code를 입력하세요.
+// var nums = [100, 200, 300, 400, 500];
 
-nums.pop();
-nums.pop();
+// nums.pop();
+// nums.pop();
 
-console.log(nums);
+// console.log(nums);
 
-// #2. 배열의 내장함수
-var arr = [200, 100, 300];
+// // #2. 배열의 내장함수
+// var arr = [200, 100, 300];
 
-arr.splice(2, 0, 10000);
+// arr.splice(2, 0, 10000);
 
-console.log(arr);  // 출력 [200, 100, 10000, 300]되게 해라.
+// console.log(arr);  // 출력 [200, 100, 10000, 300]되게 해라.
 
-// #3. 변수의 타입
-var arr = [100, 200, 300];
-console.log(typeof(arr));  // 정답 object
+// // #3. 변수의 타입
+// var arr = [100, 200, 300];
+// console.log(typeof(arr));  // 정답 object
 
-// #4. 변수의 타입2
+// // #4. 변수의 타입2
 
-// 다음 변수 a를 `typeof(a)`로 넣었을 때 출력될 값과의 연결이 알맞지 않은 것은?
-// 1)  입력 : a =1,   출력 : number
-// 2)  입력 : a = 2.22,   출력 : boolean   -> Number
-// 3)  입력 : a = 'p',   출력 : string
-// 4)  입력 : a = [1, 2, 3],   출력 : object
+// // 다음 변수 a를 `typeof(a)`로 넣었을 때 출력될 값과의 연결이 알맞지 않은 것은?
+// // 1)  입력 : a =1,   출력 : number
+// // 2)  입력 : a = 2.22,   출력 : boolean   -> Number
+// // 3)  입력 : a = 'p',   출력 : string
+// // 4)  입력 : a = [1, 2, 3],   출력 : object
 
-// #5. for문 계산
-var a = 10;
-var b = 2;
+// // #5. for문 계산
+// var a = 10;
+// var b = 2;
 
-for(var i=1; i<5; i+=2){
-    a += i;
-}
+// for(var i=1; i<5; i+=2){
+//     a += i;
+// }
 
-// console.log(a+b); 의 값은 ? 
-// i가 1일때 a는 10 + 1 = 11
-// i가 3일때 a는 11 + 3 = 14
-// i가 5면 반복문 종료. 
-// a+b는 14+2로 정답 16.
+// // console.log(a+b); 의 값은 ? 
+// // i가 1일때 a는 10 + 1 = 11
+// // i가 3일때 a는 11 + 3 = 14
+// // i가 5면 반복문 종료. 
+// // a+b는 14+2로 정답 16.
 
-// #6. False
-// 다음은 자바스크립트 문법 중에서 False로 취급하는 것들 입니다.
-// 앗, False로 취급하지 않는 것이 하나 있네요! True를 찾아주세요.
-// 1)  NaN
-// 2)  1    -> true
-// 3)  ""
-// 4)  0
-// 5)  undefined
+// // #6. False
+// // 다음은 자바스크립트 문법 중에서 False로 취급하는 것들 입니다.
+// // 앗, False로 취급하지 않는 것이 하나 있네요! True를 찾아주세요.
+// // 1)  NaN
+// // 2)  1    -> true
+// // 3)  ""
+// // 4)  0
+// // 5)  undefined
 
-// #7. 변수명
-// 다음 중 변수명으로 사용할 수 없는 것 2개를 고르시오.
-// 1)  age
-// 2)  Age
-// 3)  let    -> 예약어
-// 4)  _age
-// 5)  1age   -> 숫자로 시작 불가. 문자/밑줄(_)/달러기호($)로만 시작
+// // #7. 변수명
+// // 다음 중 변수명으로 사용할 수 없는 것 2개를 고르시오.
+// // 1)  age
+// // 2)  Age
+// // 3)  let    -> 예약어
+// // 4)  _age
+// // 5)  1age   -> 숫자로 시작 불가. 문자/밑줄(_)/달러기호($)로만 시작
 
-// #8. 객체의 키 이름 중복
-var d = {
-    'height':180,
-    'weight':78,
-    'weight':84,
-    'temperature':36,
-    'eyesight':1
-};
+// // #8. 객체의 키 이름 중복
+// var d = {
+//     'height':180,
+//     'weight':78,
+//     'weight':84,
+//     'temperature':36,
+//     'eyesight':1
+// };
 
-// console.log(d['weight']); 의 출력값은 ? 78? 땡
+// // console.log(d['weight']); 의 출력값은 ? 78? 땡
 
-// #9. concat을 활용한 출력 방법
-var year = '2019';
-var month = '04';
-var day = '26';
-var hour = '11';
-var minute = '34';
-var second = '27';
+// // #9. concat을 활용한 출력 방법
+// var year = '2019';
+// var month = '04';
+// var day = '26';
+// var hour = '11';
+// var minute = '34';
+// var second = '27';
 
-var result = year.concat('/',month,'/',day, ' ', hour, ':',minute,':',second)
+// var result = year.concat('/',month,'/',day, ' ', hour, ':',minute,':',second)
 
-console.log(result);  // 2019/04/26 11:34:27라고 출력되게하기
-
-
-// #10. 인풋모름 패스
+// console.log(result);  // 2019/04/26 11:34:27라고 출력되게하기
 
 
+// // #10. 인풋모름 패스
 
-// # 11. for을 이용한 기본 활용
-// 1부터 100까지 모두 더하는 코드를 for를 사용하여작성해라
-let s = 0;
 
-for (i = 1; i <= 100; i++) {
-    s += i
-    // console.log(i)
-}
 
-console.log(s);
+// // # 11. for을 이용한 기본 활용
+// // 1부터 100까지 모두 더하는 코드를 for를 사용하여작성해라
+// let s = 0;
 
-// # 12. 게임 캐릭터 클래스 만들기
-class Wizard {
-    constructor(health, mana, armor) {
-        this.health = health;
-        this.mana = mana;
-        this.armor = armor;
-    }
-    attack() {
-        console.log('파이어볼')
-    }
-}
+// for (i = 1; i <= 100; i++) {
+//     s += i
+//     // console.log(i)
+// }
 
-const x = new Wizard(545, 210, 10);
-console.log(x.health, x.mana, x.armor);
-x.attack();
+// console.log(s);
+
+// // # 12. 게임 캐릭터 클래스 만들기
+// class Wizard {
+//     constructor(health, mana, armor) {
+//         this.health = health;
+//         this.mana = mana;
+//         this.armor = armor;
+//     }
+//     attack() {
+//         console.log('파이어볼')
+//     }
+// }
+
+// const x = new Wizard(545, 210, 10);
+// console.log(x.health, x.mana, x.armor);
+// x.attack();
 
 // # 13. 몇번째 행성인가요?
 // let arr_13 = ['수성', '금성', '지구', '화성', '목성', '토성', '천왕성', '해왕성']
@@ -154,16 +154,16 @@ x.attack();
 
 // #18. 평균 점수
 // 내가 푼 (지저분한) 풀이
-const scores = prompt('점수 입력 : ');
+// const scores = prompt('점수 입력 : ');
 
-const [korean, math, english]= scores.split(' ');
-// console.log(korean, math, english);    제대로 담겼다.
+// const [korean, math, english]= scores.split(' ');
+// // console.log(korean, math, english);    제대로 담겼다.
 
-const score_numbers = Number(korean) + Number(math) + Number(english)
+// const score_numbers = Number(korean) + Number(math) + Number(english)
 
-const avg = score_numbers / 3
+// const avg = score_numbers / 3
 
-console.log(Math.floor(avg))    // 소수자리 버림
+// console.log(Math.floor(avg))    // 소수자리 버림
 
 
 // 정답 풀이
@@ -175,3 +175,13 @@ console.log(Math.floor(avg))    // 소수자리 버림
 // }
 
 // console.log(Math.floor(sum/3));
+
+// # 19. 제곱을 구하자
+const numbers = prompt('a와 b 입력 : ').split(' ')   // 공백으로 숫자 나누기
+
+// const a = parseInt(numbers[0], 10);
+// const b = parseInt(numbers[1], 10);
+
+// console.log(Math.pow(a, b));
+// 코드 줄이기
+console.log(Math.pow(parseInt(numbers[0], 10), parseInt(numbers[1], 10)))
