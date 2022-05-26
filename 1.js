febccd99e4c432ef3fa4855c788ddcea0740d60e
Jeon, Eun Hyung <78359232+ooooo5h@users.noreply.github.com>
2022-05-26 22:59:35 +0900

    #27 : 틀림

--- 1.js
+++ 1.js
@@ -228,16 +228,29 @@
 
 // # 26. 행성문제2
 
-const planets  = {
-    '수성' : 'Mercury',
-    '금성' : 'Venus',
-    '지구' : 'Earth',
-    '화성' : 'Mars',
-    '목성' : 'Jupiter',
-    '토성' : 'Saturn',
-    '천왕성' : 'Uranus',
-    '해왕성' : 'Neptune',
-}
+// const planets  = {
+//     '수성' : 'Mercury',
+//     '금성' : 'Venus',
+//     '지구' : 'Earth',
+//     '화성' : 'Mars',
+//     '목성' : 'Jupiter',
+//     '토성' : 'Saturn',
+//     '천왕성' : 'Uranus',
+//     '해왕성' : 'Neptune',
+// }
 
-const input = prompt('행성 한글 이름 : ');
-console.log(planets [input]);
+// const input = prompt('행성 한글 이름 : ');
+// console.log(planets [input]);
+
+// # 27. 객체 만들기
+const keys = prompt('이름 : ').split(' ');    // 이름 공백으로 나누기
+const values = prompt('점수 : ').split(' ');  // 점수 공백으로 나누기
+
+const obj = {};  // 먼저 객체를 생성하고
+
+// 반복문으로 돌면서 추가해주자.
+for(let i = 0; i < keys.length; i++) {
+    obj[keys[i]] = parseInt(values[i], 10);    // 점수는 숫자가 아니니까 parseInt로 변환
+};
+
+console.log(obj);
