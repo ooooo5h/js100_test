0b97eebc1da73addf94a25c4dea07c8d6a6a9a41
Jeon, Eun Hyung <78359232+ooooo5h@users.noreply.github.com>
2022-05-26 21:27:59 +0900

    #12

--- 1.js
+++ 1.js
@@ -94,3 +94,21 @@ for (i = 1; i <= 100; i++) {
 }
 
 console.log(s);
+
+// # 12. 게임 캐릭터 클래스 만들기
+class Wizard {
+    constructor(health, mana, armor) {
+        this.health = health;
+        this.mana = mana;
+        this.armor = armor;
+    }
+    attack() {
+        console.log('파이어볼')
+    }
+}
+
+const x = new Wizard(545, 210, 10);
+console.log(x.health, x.mana, x.armor);
+x.attack();
+
+
